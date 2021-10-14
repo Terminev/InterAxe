c933b6cb39a4d27a397cef3adaf89220cc85e15b
gauthe <eole.gauthe@edu.devinci.fr>
2021-10-14 11:28:17 +0200

    fin articles

---
+++ components/containerInformation.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div v-for="(information, index) in informations" :key="index">
+      <NuxtLink :to="'/informations/' + information.id" class="article1">
+        <img class="im" :src="information.img" alt="" />
+        <!-- <div class="im"></div> -->
+        <div class="text">
+          <h3>{{ information.title }}</h3>
+          <p>{{ information.description }}</p>
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      informations: [],
+    };
+  },
+  mounted() {
+    this.$axios.$get("http://localhost:3003/Informations").then((resp) => {
+      console.log(resp);
+      this.informations = resp;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.div3a {
+  background-color: rgb(66, 9, 9, 0.4);
+  margin-top: -30px;
+  z-index: 3;
+  text-align: center;
+
+  /* margin-top: -60px; */
+}
+/* .article {
+  display: flex;
+  justify-content: space-between;
+} */
+.article1 {
+  display: flex;
+  justify-content: start;
+  margin-bottom: 20px;
+}
+.im {
+  width: 150px;
+  height: 80px;
+  /* background-color: yellow; */
+  /* padding-top: 10px; */
+  border-radius: 20px;
+  /* margin-top: 10px; */
+  margin-left: 20px;
+}
+h3 {
+  font-size: 20px;
+}
+.text {
+  margin-left: 10px;
+  width: 500px;
+  max-width: 100%;
+  font-size: 15px;
+}
+</style>
+
+
--- pages/informations/_information.vue
+++ pages/informations/_information.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="loading">
     <div class="div1">
       <h2>LUTÈCE</h2>
       <button class="monb">S'abonner</button>
@@ -13,42 +13,21 @@
         <li>OPINION</li>
       </ul>
     </div>
-    <p>Disparitions</p>
-    <h1>Accident mortel sur l'autoroute A7</h1>
-    <p>
-      Un véhicule immatriculé 78 a provoqué un grave accident sur l'autoroute A7
-      en fin de journée.
-    </p>
-    <p>Par Mattéo Gonzalez</p>
-    <p>Publié Il y a 3 mois</p>
 
-    <img class="foot" src="../../assets/img/accident.jpg" alt="" />
+    <p>{{ information[0].title }}</p>
+    <h1>{{ information[0].description }}</h1>
+    <p>Par {{ information[0].author }}</p>
+    <p>Publié le {{ information[0].date }}</p>
 
-    <p>
-      La police municipale a confirmé en début de soirée la mort du passager
-      décédé sur le coup, et le transport en urgence du conducteur vers
-      l'hôpital le plus proche. Plusieurs blessés sont recensés dont un avec des
-      blessures graves mais dans un état stable et deux autres avec des
-      blessures légères et des dégâts mineurs sur leurs véhicules
-    </p>
+    <img class="foot" :src="information[0].img" alt="" />
 
-    <p>
-      Le parquet des Yvelines nous a confirmé qu'il s'agissait d'un accident due
-      à un problème lié au conducteur du premier véhicule qui semble avoir eu un
-      malaise entrainant la perte de connaissance et la dérive du véhicule sur
-      la voix d'en face. Le conducteur a été placé en réanimation puis en comma
-      artificielle après que son état se soit stabilisé. Les médecins ne sont
-      pas unanimes sur le fait que le patient puisse se réveiller un jour, mais
-      signales tout de même une activité cérébrale très importante.
-    </p>
+    <p>{{ information[0].content }}</p>
 
-    <p>
-      On dénombre trois accidents majeurs sur cette ligne droite depuis le début
-      de l'année. Des comptes ont étés demandés à la police nationale chargée de
-      la surveillance des voix qui déplore de son coté un manque d'effectif et
-      une diminutions des moyens financiers suites aux dernières restrictions
-      budgétaires visant à diminuer les dépenses publiques.
-    </p>
+    <p>{{ information[0].content1 }}</p>
+
+    <p>{{ information[0].content2 }}</p>
+
+    <p>{{ information[0].content3 }}</p>
 
     <footer>
       <img class="foot" src="../../assets/img/Footer.png" alt="" />
@@ -57,6 +36,24 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      information: [],
+      loading: false,
+    };
+  },
+  async mounted() {
+    var id = this.$route.params.information;
+    console.log(id);
+    var informationGet = await this.$axios.$get(
+      "http://localhost:3003/Informations?id=" + id
+    );
+    this.information = informationGet;
+    this.loading = true;
+    console.log(this.information);
+  },
+};
 </script>
 
 <style scoped>
@@ -80,5 +77,6 @@
 .foot {
   width: 100%;
   height: auto;
+  margin-bottom: 6©©0px;
 }
 </style>
