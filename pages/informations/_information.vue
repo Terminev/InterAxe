4789850aae2609b6daa776ffd158f7026b1f87a0
gauthe <eole.gauthe@edu.devinci.fr>
2021-10-14 15:51:01 +0200

    mes articles

--- pages/informations/_information.vue
+++ pages/informations/_information.vue
@@ -7,27 +7,27 @@
 
     <div class="menu">
       <ul>
-        <li>ACTUALITÉS</li>
-        <li>ECONOMIE</li>
-        <li>VIDÉOS</li>
-        <li>OPINION</li>
+        <li>ACTUALITÉS <i class="arrow down"></i></li>
+        <li>ECONOMIE <i class="arrow down"></i></li>
+        <li>VIDÉOS <i class="arrow down"></i></li>
+        <li>OPINION <i class="arrow down"></i></li>
       </ul>
     </div>
 
-    <p>{{ information[0].title }}</p>
+    <p class="title">{{ information[0].title }}</p>
     <h1>{{ information[0].description }}</h1>
-    <p>Par {{ information[0].author }}</p>
-    <p>Publié le {{ information[0].date }}</p>
+    <p class="author">Par {{ information[0].author }}</p>
+    <p class="date">Publié le {{ information[0].date }}</p>
 
     <img class="foot" :src="information[0].img" alt="" />
 
-    <p>{{ information[0].content }}</p>
+    <p class="content">{{ information[0].content }}</p>
 
-    <p>{{ information[0].content1 }}</p>
+    <p class="content">{{ information[0].content1 }}</p>
 
-    <p>{{ information[0].content2 }}</p>
+    <p class="content">{{ information[0].content2 }}</p>
 
-    <p>{{ information[0].content3 }}</p>
+    <p class="content">{{ information[0].content3 }}</p>
 
     <footer>
       <img class="foot" src="../../assets/img/Footer.png" alt="" />
@@ -57,15 +57,38 @@ export default {
 </script>
 
 <style scoped>
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 5px;
+  margin-bottom: 2px;
+  margin-left: 2px;
+}
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+h1 {
+  padding-left: 2%;
+  padding-top: 5%;
+  font-size: 30px;
+}
 .div1 {
   display: flex;
   justify-content: space-around;
-  margin: 70px;
+  background-color: #f9f9f9;
+  padding: 15px 5px;
+  margin-bottom: 20px;
 }
 .monb {
   background-color: #505dff;
   border-radius: 50px;
-  padding: 5px 10px;
+  padding: 10px 10px;
+  color: white;
+  border-style: none;
+  /* width: 100px;
+  height: 30px; */
 }
 .menu ul {
   display: flex;
@@ -77,6 +100,26 @@ export default {
 .foot {
   width: 100%;
   height: auto;
-  margin-bottom: 6©©0px;
+  margin-bottom: 60px;
+}
+
+.author {
+  padding-left: 2%;
+  padding-top: 5%;
+  color: grey;
+}
+.date {
+  padding-left: 2%;
+  color: grey;
+}
+.title {
+  padding-left: 2%;
+  color: black;
+}
+.content {
+  text-align: justify;
+  margin-left: 5%;
+  margin-right: 8%;
+  color: black;
 }
 </style>
--- pages/informations/index.vue
+++ pages/informations/index.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
     <div class="div1">
+      <div class="burgerb">
+        <span></span>
+        <span></span>
+      </div>
       <h2>LUTÈCE</h2>
       <button class="monb">S'abonner</button>
     </div>
 
     <div class="menu">
       <ul>
-        <li>ACTUALITÉS</li>
-        <li>ECONOMIE</li>
-        <li>VIDÉOS</li>
-        <li>OPINION</li>
+        <li>ACTUALITÉS <i class="arrow down"></i></li>
+        <li>ECONOMIE <i class="arrow down"></i></li>
+        <li>VIDÉOS <i class="arrow down"></i></li>
+        <li>OPINION <i class="arrow down"></i></li>
       </ul>
     </div>
     <h1>Toute l'actualité</h1>
-    <br />
-    <br />
 
     <div class="div3">
       <img class="foot" src="../../assets/img/foot.jpg" alt="" />
       <div class="div3a">Le XV de France s'impose face aux Allblacks</div>
     </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+
     <div class="article">
       <container-information />
     </div>
@@ -40,15 +38,24 @@
 </script>
 
 <style scoped>
+.burgerb span {
+  width: 50px;
+  height: 2px;
+  background-color: black;
+}
 .div1 {
   display: flex;
   justify-content: space-around;
-  margin: 70px;
+  background-color: #f9f9f9;
+  padding: 15px 5px;
+  margin-bottom: 20px;
 }
 .monb {
   background-color: #505dff;
   border-radius: 50px;
   padding: 5px 10px;
+  color: white;
+  border-style: none;
 }
 .menu ul {
   display: flex;
@@ -56,16 +63,32 @@
   list-style-type: none;
   margin-left: -30px;
 }
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 5px;
+  margin-bottom: 2px;
+  margin-left: 2px;
+}
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
 h1 {
   padding-left: 2%;
   padding-top: 5%;
   font-size: 30px;
+  margin-bottom: 30px;
 }
 
 .foot {
   width: 100%;
   height: auto;
   top: 0;
+}
+.div3 {
+  margin-bottom: 50px;
 }
 
 .div3a {
